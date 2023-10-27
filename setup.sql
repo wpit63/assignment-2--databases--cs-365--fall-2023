ed2240d87dd2be2c7042ead63fa9ab842a47f36f
wpit63 <73858845+wpit63@users.noreply.github.com>
2023-10-27 12:34:35 -0400

    Update setup.sql
    this doesn't work

--- setup.sql
+++ setup.sql
@@ -35,7 +35,7 @@ CREATE TABLE user (
 CREATE TABLE pass (
     website_URL VARCHAR(128) NOT NULL,
     username VARCHAR(64) NOT NULL,
-    p_word VARCHAR(128) NOT NULL,
+    p_word VARBINARY(128) NOT NULL,
     comment CHAR(240),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (website_URL, username, p_word)
@@ -50,7 +50,7 @@ INSERT INTO website VALUES ('https://www.apple.com', 'Apple');
 INSERT INTO website VALUES ('https://www.mozilla.org', 'Mozilla');
 INSERT INTO website VALUES ('https://www.blogger.com', 'Blogger');
 INSERT INTO website VALUES ('https://arca.live', 'Arcalive');
-INSERT INTO website VALUES ('https://arca.live', 'Arcalive'); -- note: make this a different account/ person
+INSERT INTO website VALUES ('https://www.reddit.com', 'Reddit');
 
 INSERT INTO user VALUES ('https://www.youtube.com', 'Billy', 'Bob', 'Billy Channel', 'billy@gmail.com');
 INSERT INTO user VALUES ('https://www.twitter.com', 'Victor', 'Mays', 'vmays90', 'victormays@gmail.com');
@@ -61,15 +61,15 @@ INSERT INTO user VALUES ('https://www.apple.com', 'Amelia', 'Watson', 'acedetect
 INSERT INTO user VALUES ('https://www.mozilla.org', 'Serika', 'Kuromi', 'plsnoscam', 'catsaysmeow@hotmail.com');
 INSERT INTO user VALUES ('https://www.blogger.com', 'Jason', 'Bright', 'rocketman', 'rocketman@gmail.com');
 INSERT INTO user VALUES ('https://arca.live', 'John', 'Smith', 'khajituser', 'jdoe@gmail.com');
-INSERT INTO user VALUES ('https://arca.live', 'Sue', 'Hong', 'nodeuser', 'shong@outlook.com');
+INSERT INTO user VALUES ('https://www.reddit.com', 'Jack', 'Mann', 'redditor', 'jmann@outlook.com');
 
-INSERT INTO pass VALUES ('https://www.youtube.com', 'Billy Channel', AES_ENCRYPT('bobrules!', @key_str, @init_vector, '', @salt), 'My YouTube Channel Account');
-INSERT INTO pass VALUES ('https://www.twitter.com', 'vmays90', AES_ENCRYPT('stay_OUT', @key_str, @init_vector, '', @salt), '5/70 alternative accounts');
-INSERT INTO pass VALUES ('https://github.com', 'empress5', AES_ENCRYPT('relaire', @key_str, @init_vector, '', @salt), 'why do i have this');
-INSERT INTO pass VALUES ('https://www.linkedin.com', 'Mari_G', AES_ENCRYPT('sunny', @key_str, @init_vector, '', @salt), 'job pls');
-INSERT INTO pass VALUES ('https://www.microsoft.com', 'starlight_actress', AES_ENCRYPT('mr_white', @key_str, @init_vector, '', @salt), '');
-INSERT INTO pass VALUES ('https://www.apple.com', 'acedetective', AES_ENCRYPT('mythw4tch', @key_str, @init_vector, '', @salt), 'for work');
-INSERT INTO pass VALUES ('https://www.mozilla.org', 'plsnoscam', AES_ENCRYPT('mastershiba', @key_str,  @init_vector, '', @salt), '');
-INSERT INTO pass VALUES ('https://www.blogger.com', 'rocketman', AES_ENCRYPT('glowing1', @key_str, @init_vector, '', @salt), '');
-INSERT INTO pass VALUES ('https://arca.live', 'khajituser', AES_ENCRYPT('letmein', @key_str, @init_vector, '', @salt), 'outsider');
-INSERT INTO pass VALUES ('https://arca.lvie', 'nodeuser', AES_ENCRYPT('idiottalk', @key_str, @init_vector, '', @salt), 'insider');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.youtube.com', 'Billy Channel', AES_ENCRYPT('bobrules!', @key_str, @init_vector), 'My YouTube Channel Account');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.twitter.com', 'vmays90', AES_ENCRYPT('stay_OUT', @key_str, @init_vector), '5/70 alternative accounts');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://github.com', 'empress5', AES_ENCRYPT('relaire', @key_str, @init_vector), 'why do i have this');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.linkedin.com', 'Mari_G', AES_ENCRYPT('sunny', @key_str, @init_vector), 'job pls');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.microsoft.com', 'starlight_actress', AES_ENCRYPT('mr_white', @key_str, @init_vector), '');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.apple.com', 'acedetective', AES_ENCRYPT('mythw4tch', @key_str, @init_vector), 'for work');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.mozilla.org', 'plsnoscam', AES_ENCRYPT('mastershiba', @key_str,  @init_vector), '');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.blogger.com', 'rocketman', AES_ENCRYPT('glowing1', @key_str, @init_vector), '');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://arca.live', 'khajituser', AES_ENCRYPT('letmein', @key_str, @init_vector), 'outsider');
+INSERT INTO pass (website_URL, username, p_word, comment) VALUES ('https://www.reddit.com', 'redditor', AES_ENCRYPT('idiottalk', @key_str, @init_vector), '');
